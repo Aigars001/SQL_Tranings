c8ab62e0a8cb70e880080a288e94b932a887ef6b
Aigars Maskalans <aigars545@gmail.com>
2022-09-21 19:39:20 +0300

    test 5 done

--- sql-shopify/test/05-integrity.test.ts
+++ sql-shopify/test/05-integrity.test.ts
@@ -14,7 +14,8 @@ describe("Foreign Keys", () => {
 
     it("should not be able to delete category if any app is linked", async done => {
         const categoryId = 6;
-        const query = `todo`;
+        const query = `DELETE FROM CATEGORIES
+        WHERE category_id = ${categoryId}`;
         try {
             await db.delete(query);
           } catch (e) {}
@@ -27,7 +28,8 @@ describe("Foreign Keys", () => {
 
     it("should not be able to delete pricing plan if any app is linked", async done => {
         const pricingPlanId = 100;
-        const query = `todo`;
+        const query = `DELETE FROM PRICING_PLANS
+        WHERE pricing_plan_id = ${pricingPlanId}`;
 
         try {
             await db.delete(query);
@@ -41,7 +43,8 @@ describe("Foreign Keys", () => {
 
     it("should not be able to delete app if any data is linked", async done => {
         const appId = 245;
-        const query = `todo`;
+        const query = `DELETE FROM APPS
+        WHERE app_id = ${appId}`;
 
         try {
             await db.delete(query);
@@ -55,7 +58,11 @@ describe("Foreign Keys", () => {
 
     it("should be able to delete app", async done => {
         const appId = 355;
-        const query = `todo`;
+        const query = `DELETE FROM apps_categories WHERE app_id=${appId};
+        DELETE FROM apps_pricing_plans WHERE app_id=${appId};
+        DELETE FROM key_benefits WHERE app_id=${appId};
+        DELETE FROM reviews WHERE app_id=${appId};
+        DELETE FROM apps WHERE id=${appId};`;
         try {
             await db.delete(query);
           } catch (e) {}
